116ad52da86e52a7d7a2069648d63514d724bbec
pch8388 <pch838811@gmail.com>
2020-12-18 00:15:22 +0900

    [test] 테스트 케이스 추가

--- src/test/java/me/study/base/compare/CompareTest.java
+++ src/test/java/me/study/base/compare/CompareTest.java
@@ -1,17 +1,23 @@
 package me.study.base.compare;
 
+import static java.util.stream.Collectors.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CompareTest {
 
 	@Test
+	@DisplayName("기본형 정렬")
 	void sort_primitive() {
 		int[] arr = new int[] {9, 3, 1, 4, 5, 8};
 		int[] expected = new int[] {1, 3, 4, 5, 8, 9};
@@ -21,6 +27,7 @@ public class CompareTest {
 	}
 
 	@Test
+	@DisplayName("문자열 정렬")
 	void sort_string() {
 		String[] arr = new String[] {"a", "c", "e", "k", "d"};
 		String[] expected = new String[] {"a", "c", "d", "e", "k"};
@@ -30,13 +37,31 @@ public class CompareTest {
 	}
 
 	@Test
-	void sort_object() {
+	@DisplayName("Comparable 을 구현하지 않은 객체는 예외 발생")
+	void sort_object_exception() {
 		assertThrows(ClassCastException.class,
 			() -> Arrays.sort(IntStream.range(0, 10)
 				.mapToObj(NotCompare::new).toArray()));
 	}
 
 	@Test
+	@DisplayName("Comparable 을 구현한 객체의 정렬")
+	void sort_object() {
+		List<Compare> compares = IntStream.range(0, 10)
+			.mapToObj(Compare::new).collect(toList());
+
+		List<Compare> expected = new ArrayList<>();
+		for (int i = 9; i >= 0; i--) {
+			expected.add(new Compare(i));
+		}
+
+		Collections.sort(compares);
+
+		assertEquals(expected, compares);
+	}
+
+	@Test
+	@DisplayName("함수를 통해 정렬")
 	void sort_comparator_implements() {
 		List<Integer> numbers = Arrays.asList(3, 9, -1, 7, 6);
 		List<Integer> expected = Arrays.asList(9, 7, 6, 3, -1);
@@ -46,6 +71,7 @@ public class CompareTest {
 	}
 
 	@Test
+	@DisplayName("함수를 통해 정렬조건을 여러개 넣기")
 	void sort_thenComparing() {
 		List<Person> people = Arrays.asList(
 			new Person(10, "bcd"),
