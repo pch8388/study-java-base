4b584a4a0d004c34ec4a2f98aade6acba1f4bacd
pch8388 <pch838811@gmail.com>
2021-01-07 13:22:44 +0900

    [feat] 정적팩토리 메서드 학습

---
+++ src/main/java/me/study/base/effective/item1/IntegerFactory.java
@@ -0,0 +1,27 @@
+package me.study.base.effective.item1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class IntegerFactory {
+
+	private static final Map<Integer, IntegerFactory> cache = new HashMap<>();
+
+	private int data;
+
+	static {
+		IntStream.range(0, 101).forEach(i -> cache.put(i, new IntegerFactory(i)));
+	}
+
+	private IntegerFactory(int data) {
+		this.data = data;
+	}
+
+	public static IntegerFactory valueOf(int data) {
+		if (cache.containsKey(data)) {
+			return cache.get(data);
+		}
+		return new IntegerFactory(data);
+	}
+}
