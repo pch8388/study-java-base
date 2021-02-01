ae27e1a3a1cdfe86fb3d88bcd587b54aef0ecd45
pch8388 <pch838811@gmail.com>
2021-02-01 11:16:56 +0900

    [feat] 의존성 주입 학습

---
+++ src/main/java/me/study/base/effective/item4/SpellChecker.java
@@ -0,0 +1,54 @@
+package me.study.base.effective.item4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class SpellChecker {
+	 private final Lexicon dictionary;
+
+	public SpellChecker(Supplier<Lexicon> dictionary) {
+		this.dictionary = Objects.requireNonNull(dictionary.get());
+	}
+
+	public boolean isValid(String word) {
+		return dictionary.isValid(word);
+	}
+}
+
+interface Lexicon {
+	boolean isValid(String word);
+}
+
+class EnglishLexicon implements Lexicon {
+	private final static Map<String, String> words;
+
+	static {
+		words = new HashMap<>();
+		words.put("a", "word - a");
+		words.put("b", "word - b");
+		words.put("c", "word - c");
+		words.put("d", "word - d");
+	}
+
+	public boolean isValid(String word) {
+		return words.containsKey(word);
+	}
+}
+
+class KoreanLexicon implements Lexicon {
+	private final static Map<String, String> words;
+
+	static {
+		words = new HashMap<>();
+		words.put("가", "word - 가");
+		words.put("나", "word - 나");
+		words.put("다", "word - 다");
+		words.put("라", "word - 라");
+	}
+
+	public boolean isValid(String word) {
+		return words.containsKey(word);
+	}
+}
---
+++ src/test/java/me/study/base/effective/item4/SpellCheckerTest.java
@@ -0,0 +1,15 @@
+package me.study.base.effective.item4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpellCheckerTest {
+
+	@Test
+	void findWord() {
+		SpellChecker s = new SpellChecker(EnglishLexicon::new);
+		assertTrue(s.isValid("a"));
+		assertFalse(s.isValid("x"));
+	}
+}
