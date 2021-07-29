84938eedfb83fd6b18945f525b00bc5d0477bb84
SC <pch838811@gmail.com>
2021-07-29 23:58:14 +0900

    backpressure

--- study-reactive/src/test/java/me/reactive/study/base/SampleSubscriberTest.java
+++ study-reactive/src/test/java/me/reactive/study/base/SampleSubscriberTest.java
@@ -1,7 +1,9 @@
 package me.reactive.study.base;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 
 class SampleSubscriberTest {
@@ -16,6 +18,24 @@ class SampleSubscriberTest {
 			s -> s.request(10));
 
 		ints.subscribe(subscriber);
+	}
+
+	@Test
+	void base_subscriber_anonymous() {
+		Flux.range(1, 10)
+			.doOnRequest(r -> System.out.println("request of " + r))
+			.subscribe(new BaseSubscriber<>() {
+				@Override
+				protected void hookOnSubscribe(Subscription subscription) {
+					request(1);
+				}
+
+				@Override
+				protected void hookOnNext(Integer value) {
+					System.out.println("Cancelling after having received " + value);
+					cancel();
+				}
+			});
 	}
 
 }
