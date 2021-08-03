50b9c355ce6a800000928520e6136b257947afbd
SC <pch838811@gmail.com>
2021-08-03 23:18:22 +0900

    synchronous generate

--- study-reactive/src/test/java/me/reactive/study/base/SinkTest.java
+++ study-reactive/src/test/java/me/reactive/study/base/SinkTest.java
@@ -1,5 +1,7 @@
 package me.reactive.study.base;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
@@ -12,11 +14,21 @@ public class SinkTest {
 			() -> 1,
 			(state, sink) -> {
 				sink.next("3 x " + state + " = " + 3 * state);
-				if (state == 9)
-					sink.complete();
+				if (state == 9) sink.complete();
 				return state + 1;
 			});
 
 		generateFlux.subscribe(System.out::println);
+
+		Flux<String> longFlux = Flux.generate(
+			AtomicLong::new,
+			(state, sink) -> {
+				long i = state.getAndIncrement();
+				sink.next("5 x " + i + " = " + 5 * i);
+				if (i == 10) sink.complete();
+				return state;
+			});
+
+		longFlux.subscribe(System.out::println);
 	}
 }
