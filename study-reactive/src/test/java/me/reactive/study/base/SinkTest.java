6a477534ec5e30cf40818245c17f46ebc01afd03
SC <pch838811@gmail.com>
2021-08-03 23:24:45 +0900

    synchronous generate

--- study-reactive/src/test/java/me/reactive/study/base/SinkTest.java
+++ study-reactive/src/test/java/me/reactive/study/base/SinkTest.java
@@ -2,12 +2,14 @@ package me.reactive.study.base;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
 
 public class SinkTest {
 
+	@DisplayName("평범한 사용")
 	@Test
 	void generate() {
 		Flux<String> generateFlux = Flux.generate(
@@ -19,8 +21,12 @@ public class SinkTest {
 			});
 
 		generateFlux.subscribe(System.out::println);
+	}
 
-		Flux<String> longFlux = Flux.generate(
+	@DisplayName("state 가 변할 경우")
+	@Test
+	void generate_mutable_state() {
+		Flux<String> mutableStateFlux = Flux.generate(
 			AtomicLong::new,
 			(state, sink) -> {
 				long i = state.getAndIncrement();
@@ -29,6 +35,23 @@ public class SinkTest {
 				return state;
 			});
 
-		longFlux.subscribe(System.out::println);
+		mutableStateFlux.subscribe(System.out::println);
+	}
+
+	@DisplayName("Consumer 사용")
+	@Test
+	void generate_with_consumer() {
+		Flux<String> generateConsumer = Flux.generate(
+			AtomicLong::new,
+			(state, sink) -> {
+				long i = state.getAndIncrement();
+				sink.next("6 x " + i + " = " + 6 * i);
+				if (i == 10)
+					sink.complete();
+				return state;
+			}, (state) -> System.out.println("state : " + state));
+
+		// Consumer 가 상태 값의 처리가 끝난 후의 처리를 담당한다
+		generateConsumer.subscribe(System.out::println);
 	}
 }
