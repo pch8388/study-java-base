13004f88b2ec283d9defa8b0fafbe20fffa996bb
SC <pch838811@gmail.com>
2021-08-10 01:06:13 +0900

    using

--- study-reactive/src/test/java/me/reactive/study/base/ErrorHandleTest.java
+++ study-reactive/src/test/java/me/reactive/study/base/ErrorHandleTest.java
@@ -1,8 +1,11 @@
 package me.reactive.study.base;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 public class ErrorHandleTest {
@@ -37,6 +40,31 @@ public class ErrorHandleTest {
 			.onErrorReturn(e -> e.getMessage().equals("doSomethingException"), "RECOVERED");
 
 		flux.subscribe(value -> System.out.println("RECEIVED " + value));
+	}
+
+	@Test
+	void using() {
+		AtomicBoolean isDisposed = new AtomicBoolean();
+		Disposable disposableInstance = new Disposable() {
+			@Override
+			public void dispose() {
+				isDisposed.set(true);
+			}
+
+			@Override
+			public String toString() {
+				return "DISPOSABLE";
+			}
+		};
+
+		Flux<String> flux =
+			Flux.using(
+				() -> disposableInstance,
+				disposable -> Flux.just(disposable.toString()),
+				Disposable::dispose
+			);
+
+		flux.subscribe(System.out::println);
 	}
 
 	private String doSecondTransform(String s) {
