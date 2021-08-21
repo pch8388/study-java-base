f111a0a21ee8ebb8e345c67acee3e63ab895bffe
SC <pch838811@gmail.com>
2021-08-22 01:41:40 +0900

    PublisherProbe test 학습

--- study-reactive/src/test/java/me/reactive/study/testing/PublisherProbeTest.java
+++ study-reactive/src/test/java/me/reactive/study/testing/PublisherProbeTest.java
@@ -6,6 +6,7 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.test.publisher.PublisherProbe;
 
 public class PublisherProbeTest {
 
@@ -26,5 +27,26 @@ public class PublisherProbeTest {
 	static Flux<String> processOrFallback(Mono<String> source, Publisher<String> fallback) {
 		return source.flatMapMany(phrase -> Flux.fromArray(phrase.split("\\s+")))
 			.switchIfEmpty(fallback);
+	}
+
+	@Test
+	void testCommandEmptyPathIsUsed() {
+		PublisherProbe<Void> probe = PublisherProbe.empty();
+
+		StepVerifier.create(processOrFallback(Mono.empty(), probe.mono()))
+			.verifyComplete();
+
+		probe.assertWasSubscribed();
+		probe.assertWasRequested();
+		probe.assertWasNotCancelled();
+	}
+
+	static Mono<Void> processOrFallback(Mono<String> commandSource, Mono<Void> doWhenEmpty) {
+		return commandSource.flatMap(command -> executeCommand(command).then())
+			.switchIfEmpty(doWhenEmpty);
+	}
+
+	private static Mono<String> executeCommand(String command) {
+		return Mono.just(command + " DONE");
 	}
 }
