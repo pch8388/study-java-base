9eb51a3aed5ebdc63068c55560a57c337e2e0f8d
SC <pch838811@gmail.com>
2021-10-10 11:39:22 +0900

    spring init

---
+++ study-spring/src/main/java/com/example/studyspring/StudySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringApplication.class, args);
+	}
+
+}
