24afddf4a7097308726399266e99d45a476b4979
SC <pch838811@gmail.com>
2021-10-11 14:06:27 +0900

    spring - 비동기 callable 응답
    webflux 기본 netty 를 사용하면 응답을 기다리지 않고 바로 결과를 반환한다
    tomcat 을 써야 결과가 제대로 나오는 데 이유는 잘 모르겠음..

--- study-spring/src/main/java/com/example/studyspring/StudySpringApplication.java
+++ study-spring/src/main/java/com/example/studyspring/StudySpringApplication.java
@@ -1,17 +1,12 @@
 package com.example.studyspring;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
+import java.util.concurrent.Callable;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,52 +15,20 @@ import lombok.extern.slf4j.Slf4j;
 @EnableAsync
 public class StudySpringApplication {
 
-	@Component
-	static class MyService {
-		@Async
-		public ListenableFuture<String> hello()  {
-			log.info("hello()");
-
-			try {
+	@RestController
+	static class MyController {
+		@GetMapping("/callable")
+		public Callable<String> callable() {
+			log.info("callable");
+			return () -> {
+				log.info("async");
 				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				log.info(e.getMessage());
-			}
-			return new AsyncResult<>( "Hello");
+				return "hello";
+			};
 		}
-	}
-
-	@Bean
-	ThreadPoolTaskExecutor tp() {
-		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-		// 초기 풀 사이즈
-		te.setCorePoolSize(10);
-		// 쓰레드마다 할당되는 큐에 수용할 수 있는 최대 요청
-		te.setQueueCapacity(200);
-		// 초기 풀사이즈에 있는 큐가 모두 가득 차면 쓰레드수를 최대 수만큼 늘림
-		te.setMaxPoolSize(100);
-		te.setThreadNamePrefix("my-thread");
-		te.initialize();
-
-		return te;
 	}
 
 	public static void main(String[] args) {
-		try (ConfigurableApplicationContext c = SpringApplication.run(StudySpringApplication.class, args)) {
-		}
-	}
-
-	@Autowired MyService myService;
-
-	@Bean
-	ApplicationRunner run() {
-		return args -> {
-			log.info("run()");
-			ListenableFuture<String> res = myService.hello();
-			res.addCallback(
-				s -> log.info("result : " + s),
-				e -> log.info("error : " + e.getMessage()));
-			log.info("exit");
-		};
+		SpringApplication.run(StudySpringApplication.class, args);
 	}
 }
