5c97d93b752dda8e50a00203e2cc153061b8b747
SC <pch838811@gmail.com>
2021-10-22 20:15:58 +0900

    flux defer
    - defer 를 사용하지 않고 switchIfEmpty 와 같은 구문을 사용하면 미리 호출이 된다.

---
+++ study-reactive/src/main/java/me/reactive/study/base/DeferEx.java
@@ -0,0 +1,36 @@
+package me.reactive.study.base;
+
+import static me.reactive.study.base.DeferEx.Repository.*;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class DeferEx {
+	public static void main(String[] args) {
+		Flux.range(0, 5)
+			.flatMap(x -> Mono.just(x * 2))
+			.switchIfEmpty(Flux.defer(() -> getJust()))
+			.subscribe(System.out::println);
+
+		Flux.range(0, 5)
+			.flatMap(x -> Mono.just(x * 2))
+			.switchIfEmpty(getJust())
+			.subscribe(x -> System.out.println("print just : " + x));
+	}
+
+	private static Mono<Integer> getJust() {
+		return getInt();
+	}
+
+	static class Repository {
+		static Mono<Integer> getInt() {
+			try {
+				System.out.println("getInt In !!");
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return Mono.just(100);
+		}
+	}
+}
