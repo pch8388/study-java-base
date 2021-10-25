e3d3e4f50c4cda9b6d8be7ec6e95afef7d9d60c1
SC <pch838811@gmail.com>
2021-10-25 23:17:23 +0900

    DeferredResult 사용 => 언젠가 값을 반환해주는 callback

--- study-spring/src/main/java/com/example/studyspring/StudySpringApplication.java
+++ study-spring/src/main/java/com/example/studyspring/StudySpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,10 +20,19 @@ public class StudySpringApplication {
 		AsyncRestTemplate rt = new AsyncRestTemplate();
 
 		@GetMapping("/rest")
-		public ListenableFuture<ResponseEntity<String>> rest(int idx) {
-			return rt.getForEntity(
+		public DeferredResult<String> rest(int idx) {
+			// 언젠가 값을 반환해줌
+			DeferredResult<String> dr = new DeferredResult<>();
+
+			ListenableFuture<ResponseEntity<String>> f1 = rt.getForEntity(
 				"http://localhost:8083/service?req={req}", String.class,
 				"hello" + idx);
+
+			// 예외를 전파해봤자 정확하게 스프링 핸들러에서 받을지 모름 => 어떤 thread 에서 실행중인지 알 수 없기 때문
+			f1.addCallback(s -> dr.setResult(s.getBody() + "/work"),
+				e -> dr.setErrorResult(e.getMessage()));
+
+			return dr;
 		}
 	}
 
