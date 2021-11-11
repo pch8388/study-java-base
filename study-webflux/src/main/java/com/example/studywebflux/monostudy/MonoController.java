92e569fb3d57fd4585e4897c96a527ea407090a6
SC <pch838811@gmail.com>
2021-11-12 00:21:33 +0900

    webflux 실습3

--- study-webflux/src/main/java/com/example/studywebflux/monostudy/MonoController.java
+++ study-webflux/src/main/java/com/example/studywebflux/monostudy/MonoController.java
@@ -3,14 +3,24 @@ package com.example.studywebflux.monostudy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class MonoController {
 
 	@GetMapping("/")
 	Mono<String> hello() {
-		return Mono.just("Hello WebFlux").log();
+		log.info("pos1");
+		Mono<String> m = Mono.fromSupplier(this::generateHello).doOnNext(log::info).log();
+		log.info("pos2");
+		return m;
+	}
+
+	private String generateHello() {
+		log.info("method generateHello()");
+		return "Hello Mono";
 	}
 
 }
