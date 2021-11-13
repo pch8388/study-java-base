6a0e76ed63cbd0ad82f7aedf60b21cd572a5b9ff
SC <pch838811@gmail.com>
2021-11-14 00:18:48 +0900

    webflux 실습4

--- study-webflux/src/main/java/com/example/studywebflux/monostudy/MonoController.java
+++ study-webflux/src/main/java/com/example/studywebflux/monostudy/MonoController.java
@@ -1,9 +1,16 @@
 package com.example.studywebflux.monostudy;
 
+import java.util.stream.Stream;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -21,6 +28,26 @@ public class MonoController {
 	private String generateHello() {
 		log.info("method generateHello()");
 		return "Hello Mono";
+	}
+
+	@GetMapping("/event/{id}")
+	Mono<Event> event(@PathVariable long id) {
+		return Mono.just(new Event(id, "event"+id));
+	}
+
+	// text/event-stream 을 이용하려면 Flux 로 return 해야 함
+	// text/event-stream : 데이터를 stream 형태로 나눠서 보냄
+	@GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	Flux<Event> events() {
+		return Flux
+			.fromStream(Stream.generate(() -> new Event(System.currentTimeMillis(), "value")))
+			.take(10);
+	}
+
+	@Data @AllArgsConstructor
+	public static class Event {
+		long id;
+		String value;
 	}
 
 }
