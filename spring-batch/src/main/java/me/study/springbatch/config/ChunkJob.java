77439486e70e96076b373e02826444fce12d5687
sc <pch838811@gmail.com>
2022-04-16 19:54:55 +0900

    chunk job

---
+++ spring-batch/src/main/java/me/study/springbatch/config/ChunkJob.java
@@ -0,0 +1,60 @@
+package me.study.springbatch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@EnableBatchProcessing
+@Configuration
+@RequiredArgsConstructor
+public class ChunkJob {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkBasedJob() {
+        return this.jobBuilderFactory.get("chunkBasedJob")
+                .start(chunkStep())
+                .build();
+    }
+
+    @Bean
+    public Step chunkStep() {
+        return this.stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(1000)
+                .reader(itemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public ListItemReader<String> itemReader() {
+        List<String> items = new ArrayList<>(100000);
+
+        for (int i = 0; i < 100000; i++) {
+            items.add(UUID.randomUUID().toString());
+        }
+        return new ListItemReader<>(items);
+    }
+
+    @Bean
+    public ItemWriter<String> itemWriter() {
+        return items -> {
+            for (String item : items) {
+                System.out.println(">> current item = " + item);
+            }
+        };
+    }
+}
