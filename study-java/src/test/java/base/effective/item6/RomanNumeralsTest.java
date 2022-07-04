33eae2326d48aecf263530f5f9fa37e51a07d1bb
sc <pch838811@gmail.com>
2022-07-04 21:19:39 +0900

    set test

--- study-java/src/test/java/base/effective/item6/RomanNumeralsTest.java
+++ study-java/src/test/java/base/effective/item6/RomanNumeralsTest.java
@@ -3,6 +3,12 @@ package base.effective.item6;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RomanNumeralsTest {
 
 	@Test
@@ -37,5 +43,22 @@ class RomanNumeralsTest {
 		for (int i = 0; i < 1_000_000_000; i++) {
 			sum += i;
 		}
+	}
+
+	@Test
+	@DisplayName("key set 데이터 공유 확인")
+	void map_keySet() {
+		Map<String, String> map = new HashMap<>();
+		map.put("test1", "test1");
+		map.put("test2", "test2");
+
+		Set<String> preKeySet = map.keySet();
+		assertThat(preKeySet.size()).isEqualTo(2);
+
+		Set<String> keys = map.keySet();
+		keys.remove("test1");
+
+		assertThat(preKeySet == keys).isTrue();
+		assertThat(preKeySet.size()).isEqualTo(keys.size());
 	}
 }
